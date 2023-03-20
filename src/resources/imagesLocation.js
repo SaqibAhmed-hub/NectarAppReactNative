cdcd8a9d34fd80837208053ca143a5d0b1ce4053
Saqib Ahmed <saqibahmed-colan>
2023-03-20 19:07:04 +0530

    made some Changes

--- src/resources/imagesLocation.js
+++ src/resources/imagesLocation.js
@@ -4,6 +4,7 @@ const images = {
     app_icon: require('./images/nectar_icon.png'),
     fruit_img: require('./images/food_images.png'),
     color_icon: require('./images/color_icon.png'),
+    login_bg: require('./images/login_background.png'),
 };
 
 
--- src/screens/LogInScreen.js
+++ src/screens/LogInScreen.js
@@ -1,10 +1,7 @@
 import { useState } from 'react';
 import {
-    Image,
-    StyleSheet,
-    Text,
-    TextInput, View,
-    TouchableOpacity
+    Image, ImageBackground, StyleSheet,
+    Text, TextInput, TouchableOpacity, View
 } from 'react-native';
 import images from '../resources/imagesLocation';
 
@@ -16,97 +13,102 @@ const LogInScreen = ({ navigation }) => {
     return (
         <View style={LogInStyle.container}>
             <View style={LogInStyle.imgcontainer} >
-                <Image
-                    source={images.color_icon}
-                    style={LogInStyle.imgstyle}
-                />
+                <ImageBackground
+                    style={LogInStyle.imgbackground}
+                    source={images.login_bg}   >
+                    <Image
+                        source={images.color_icon}
+                        style={LogInStyle.imgstyle}
+                    />
+                </ImageBackground>
             </View>
-            <Text style={{
-                fontSize: 20,
-                fontFamily: 'Gilroy-Medium',
-                color: '#000'
+            <View style={LogInStyle.logging}>
 
-            }}>Loging </Text>
-            <Text
-                style={{
+                <Text style={{
+                    fontSize: 20,
                     fontFamily: 'Gilroy-Medium',
-                    marginTop: 10
-                }}>
-                Enter your emails and password
-            </Text>
-            <Text
-                style={{
-                    fontFamily: 'Gilroy-Bold',
-                    marginTop: 20
-                }}>
-                Email
-            </Text>
-            <TextInput
-                placeholder='Enter your Email'
-                style={LogInStyle.input}
-                onChangeText={setEmail}
-                value={email}
-                keyboardType='email-address'
-            />
-            <Text
-                style={{
-                    fontFamily: 'Gilroy-Bold',
-                    marginTop: 20
-                }}>
-                Password
-            </Text>
-            <TextInput
-                placeholder='Enter your Password'
-                style={LogInStyle.input}
-                onChangeText={SetPassword}
-                value={password}
-                secureTextEntry={true}
-                returnKeyType='go'
-            />
+                    color: '#000',
+                    fontWeight : '400'
 
-            <Text style={{
-                marginTop: 20,
-                alignItems : 'flex-end'
-            }}>Forgot Password?</Text>
-
-            <TouchableOpacity
-                onPress={() => {
-                    
-                }}
-            >
-                <View
+                }}>Loging </Text>
+                <Text
                     style={{
-                        width: 350,
-                        padding: 16,
-                        backgroundColor: '#53B175',
-                        borderRadius: 10,
-                        marginTop: 20,
-                        alignItems: 'center'
-
+                        fontFamily: 'Gilroy-Medium',
+                        marginTop: 10
                     }}>
-                    <Text
+                    Enter your emails and password
+                </Text>
+                <Text
+                    style={{
+                        fontFamily: 'Gilroy-Bold',
+                        marginTop: 20
+                    }}>
+                    Email
+                </Text>
+                <TextInput
+                    placeholder='Enter your Email'
+                    style={LogInStyle.input}
+                    onChangeText={setEmail}
+                    value={email}
+                    keyboardType='email-address'
+                    multiline = {false}
+                />
+                <Text
+                    style={{
+                        fontFamily: 'Gilroy-Bold',
+                        marginTop: 20
+                    }}>
+                    Password
+                </Text>
+                <TextInput
+                    placeholder='Enter your Password'
+                    style={LogInStyle.input}
+                    onChangeText={SetPassword}
+                    value={password}
+                    secureTextEntry={true}
+                    returnKeyType='go'
+                    maxLength={30}
+                />
+
+                <Text style={{
+                    marginTop: 20,
+                    alignSelf: 'flex-end'
+                }}>Forgot Password?</Text>
+
+                <TouchableOpacity
+                    onPress={() => {
+
+                    }}
+                >
+                    <View
                         style={{
-                            color: 'white',
-                            fontSize: 16,
-                            fontFamily: 'Gilroy-Bold'
+                            width: 350,
+                            padding: 16,
+                            backgroundColor: '#53B175',
+                            borderRadius: 10,
+                            marginTop: 20,
+                            alignSelf : 'center',
+                            alignItems : 'center'
+
                         }}>
-                        Log In
-                    </Text>
+                        <Text
+                            style={{
+                                color: 'white',
+                                fontSize: 16,
+                                fontFamily: 'Gilroy-Bold'
+                            }}>
+                            Log In
+                        </Text>
+                    </View>
 
-                </View>
+                </TouchableOpacity>
 
-            </TouchableOpacity>
-
-            <Text style= {{
-                marginTop : 20,
-                alignSelf : 'center'
-            }}>Don’t have an account?  Sign up</Text>
-
-
-
-
-
-
+                <Text style={{
+                    marginTop: 20,
+                    alignSelf: 'center'
+                }}>Don’t have an account? <Text style={{ color: '#53B175',
+                fontWeight : '300' }}> Sign up</Text></Text>
+            </View>
         </View>
     )
 }
@@ -115,19 +117,30 @@ const LogInStyle = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'flex-start',
+        backgroundColor: '#fff'
+    },
+    logging: {
         margin: 20
     },
     imgcontainer: {
-        flex: 0.3,
+        flex: 0.4,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    imgbackground: {
+        flex: 1,
+        width: '100%',
+        resizeMode: "cover",
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     imgstyle: {
         padding: 20,
+        alignSelf: 'center'
     },
     input: {
         borderBottomWidth: 1,
-        borderColor : '#E2E2E2',
+        borderColor: '#E2E2E2',
         fontSize: 16,
         fontFamily: 'Gilroy-Medium'
     }
